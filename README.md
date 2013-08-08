5067e9d86d8dff572a69d848decc31a9d86d1ea2
Julia Mailander <julia.mailander@gmail.com>
2013-08-08 14:34:12 -0700

    First commit and README update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bloccit
+
+A Reddit-esque application to demonstrate foundation programming and web development
+
+Made with RYAN at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '79049c55563e559480b136e6ad775705447291c354eb7844a5bb0c7734b46358680f037047984b793c7f4643fd95ed2a4d49266bd335877b17bc7bb080421776'
